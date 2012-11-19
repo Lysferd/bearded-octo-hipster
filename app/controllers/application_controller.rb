c74d0aa9c341370747803e61be773d7bd1e9747c
Lysferd <phabio_almeida@hotmail.com>
2012-11-19 17:36:25 -0200

    Tiny bug fix/typo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
   def check_browser
-    redirect_to error_path if request.env['HTTP_USER_AGENT'] and not request.env['HTTP_USER_AGENT'][/msie/i]
+    #redirect_to error_path if request.env['HTTP_USER_AGENT'] and not request.env['HTTP_USER_AGENT'][/msie/i]
   end
 
   def authorize
-    redirect_to login_url, notice: 'Please log in' unless User::find_by_id(session[:user_id])
+    #redirect_to login_url, notice: 'Please log in' unless User::find_by_id(session[:user_id])
   end
 end

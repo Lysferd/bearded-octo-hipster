73ce3cf4ccd02e0f4c6dbfda14d212bcd52632fa
Lysferd <phabio_almeida@hotmail.com>
2012-11-23 18:01:31 -0200

    Took installers off. Made new installer.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,59 @@
-set :application, "set your application name here"
-set :repository, "set your repository location here"
+set :user, 'ibtech'
+set :password, 'ibtechserver'
+set :application, 'videoserver3'
+set :repository, 'git@github.com:Lysferd/bearded-octo-hipster.git'
+set :deploy_to, "/home/#{user}/#{application}"
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#require 'rvm/capistrano'
+set :rvm_ruby_string, '1.9.3-p286'
+set :rvm_type, :user
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here" # Your HTTP server, Apache/etc
-role :app, "your app-server here" # This may be the same as your `Web` server
-role :db, "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db, "your slave db-server here"
+role :web, '201.64.223.142' # Your HTTP server, Apache/etc
+role :app, '201.64.223.142' # This may be the same as your `Web` server
+role :db, '201.64.223.142', :primary => true # This is where Rails migrations will run
+#role :db, "your slave db-server here"
+
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :rails_env, :production
+
+namespace :deploy do
+  desc "cause Passenger to initiate a restart"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after 'deploy:restart', 'deploy:cleanup'
+
+after "deploy:update_code", :bundle_install
+desc "install the necessary prerequisites"
+task :bundle_install, :roles => :app do
+  run "cd #{release_path} && bundle install"
+end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#namespace :deploy do
+#  task :start do
+#  end
+#  task :stop do
+#  end
+#  task :restart, :roles => :app, :except => {:no_release => true} do
+#    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+#  end
+#end
